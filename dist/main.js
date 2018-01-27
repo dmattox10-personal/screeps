68c0e3ce3b9c80bff385e3846d5e10a27bf6eea8
Daniel Mattox <dmattox10@gmail.com>
2018-01-27 15:04:04 -0500

    Changes

--- dist/main.js
+++ dist/main.js
@@ -7,8 +7,8 @@ var cleanup = require('cleanup');
 module.exports.loop = function () {
 
     cleanup.memory(Memory);
-    var lcl = Game.room.controller();
-    console.log(lcl);
+    //var lcl = Game.room.controller();
+    //console.log(lcl);
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
