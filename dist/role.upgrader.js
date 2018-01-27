bc4fb4110e1159aa95e3be12bc8c11bf59cf3f18
Daniel Mattox <dmattox10@gmail.com>
2018-01-27 12:47:27 -0500

    changes

--- dist/role.upgrader.js
+++ dist/role.upgrader.js
@@ -5,7 +5,7 @@ var roleUpgrader = {
 	    if(creep.carry.energy == 0) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+                creep.moveTo(sources[0]); // TODO these hardcoded positions need looped
             }
         }
         else {
