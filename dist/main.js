ccc0f27af63ba4e13e7e8ac137e2e0c558ce0446
Daniel Mattox <dmattox10@gmail.com>
2018-01-29 13:47:15 -0500

    Changes

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@ module.exports.loop = function () {
 
     cleanup.memory();
     for(var name in Game.rooms) { // GOOD
-      console.log(Object.keys(Game.room[0]));
+      console.log(Object.keys(Game.rooms[0]));
       if (Memory.phase < Game.rooms[name].controller.level) { // GOOD
         Memory.phase = Game.rooms[name].controller.level; // GOOD
         switch(Memory.phase) { // TODO Map the room, every time we level up!
