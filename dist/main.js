1bb9546bf9f329057ecccc532affd27ffa6034be
Daniel Mattox <dmattox10@gmail.com>
2018-01-29 13:24:17 -0500

    Changes

--- dist/main.js
+++ dist/main.js
@@ -14,7 +14,7 @@ const ROOM_WIDTH = 50;
 module.exports.loop = function () {
 
     cleanup.memory();
-    console.log(JSON.stringify(Game.rooms));
+    console.log(JSON.stringify(Game.rooms.name));
     for(var name in Game.rooms) { // GOOD
       if (Memory.phase < Game.rooms[name].controller.level) { // GOOD
         Memory.phase = Game.rooms[name].controller.level; // GOOD
