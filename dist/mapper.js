17f8e4201fba4ee1127dd476baee61a7139ccd17
Daniel Mattox <dmattox10@gmail.com>
2018-02-02 11:28:17 -0500

    small changes

--- dist/mapper.js
+++ dist/mapper.js
@@ -1,6 +1,8 @@
 var mapper = {
   createMap: function(name, spawn_name, sources) {
-    console.log(JSON.stringify(sources));
+    for (var i = 0; i < sources.length; i++) {
+    console.log(sources[i].pos);
+    }
   }, // MAIN
   todofixme: function(spawn_name, length) {
 
