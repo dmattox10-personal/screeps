e2ad809c2ed69d5642be9d59117096fbaf3b30c4
Daniel Mattox <dmattox10@gmail.com>
2018-02-02 18:09:06 -0500

    small changes

--- dist/mapper.js
+++ dist/mapper.js
@@ -1,23 +1,24 @@
 var mapper = {
 
   createMap: function(ROOM_WIDTH, ROOM_HEIGHT, name, sources) {
-    var terrain = {};
-    Memory.map = {};
+    Memory.map = [];
     Memory.map.name = name;
+    Memory.map.tiles = {};
+    var length = Memory.map.length;
     console.log(JSON.stringify(Memory.map));
     console.log(Memory.map.name);
       for (var x = 0; x < ROOM_WIDTH; x++) {
         for (var y = 0; y < ROOM_HEIGHT; y++) {
           //console.log(Game.map.getTerrainAt(x, y, name));
           let tile = Game.map.getTerrainAt(x, y, name);
-        /*  Memory.map.name.terrain[Memory.map.length + 1] = {
-            type: tile,
-            pos:
+          Memory.map.name.terrain[Memory.map.length + 1] = {
+            "type": tile,
+            "pos":
             {
-              x:x,
-              y:y
+              "x":x,
+              "y":y
             }
-          }; */
+          };
         } // 'y' loop
       } // 'x' loop
     }, // createMap
