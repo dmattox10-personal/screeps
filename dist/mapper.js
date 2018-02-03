571da3768f331c232bbbd14573c16a2d4be8e366
Daniel Mattox <dmattox10@gmail.com>
2018-02-02 23:19:34 -0500

    small changes

--- dist/mapper.js
+++ dist/mapper.js
@@ -2,7 +2,6 @@ var mapper = {
 
   createMap: function(ROOM_WIDTH, ROOM_HEIGHT, name, sources) {
     // Memory.map.name.sites // Gonna need this
-    var Memory.map.build = [];
       for (var x = 0; x < ROOM_WIDTH; x++) {
         for (var y = 0; y < ROOM_HEIGHT; y++) {
           //console.log(Game.map.getTerrainAt(x, y, name));
