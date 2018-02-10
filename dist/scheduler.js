15b440f03ecbbf68c369cb76d2e3d3e9035483f8
Daniel Mattox <dmattox10@gmail.com>
2018-02-09 22:45:34 -0500

    Changes

--- dist/scheduler.js
+++ dist/scheduler.js
@@ -3,20 +3,24 @@ var scheduler = {
     if (Game.time % 5 === 0) {
       return true;
     }
+    else {return false;}
   },
   ten: function() {
     if (Game.time % 10 === 0) {
       return true;
     }
+    else {return false;}
   },
   twentyFive: function() {
     if (Game.time % 25 === 0) {
       return true;
     }
+    else {return false;}
   },
   fifty: function() {
     if (Game.time % 5 === 0) {
       return true;
     }
+    else {return false;}
   }
 };
