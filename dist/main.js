612400c80ed062f825c85ea962cde3cdb8497ae1
Daniel Mattox <dmattox10@gmail.com>
2018-02-09 22:32:03 -0500

    Changes

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@ const ROOM_HEIGHT = 50;
 const ROOM_WIDTH = 50;
 
 module.exports.loop = function () {
-    if(scheduler.ticks5(Game.time)) {
+    if(scheduler.ticks5.test())) {
       console.log("5 TICKS!")
     }
     /*
