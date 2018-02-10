a363feeef4b6c2eff75adb77a79b4ee806cd31aa
Daniel Mattox <dmattox10@gmail.com>
2018-02-09 22:50:31 -0500

    Changes

--- dist/main.js
+++ dist/main.js
@@ -16,9 +16,8 @@ const ROOM_WIDTH = 50;
 
 module.exports.loop = function () {
 
-    if (scheduler.five(Game.time)) {
-      console.log("FIVE TICKS!")
-    }
+    var five = scheduler.five(Game.time);
+    console.log(five);
     //console.log(Game.time);
     //console.log(scheduler.ten);
     cleanup.memory();
