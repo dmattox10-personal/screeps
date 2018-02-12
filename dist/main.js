f927f3e99feab717c586a12d51f582f83adada3c
Daniel Mattox <dmattox10@gmail.com>
2018-02-12 17:59:43 -0500

    small changes

--- dist/main.js
+++ dist/main.js
@@ -15,6 +15,7 @@ const ROOM_HEIGHT = 50;
 const ROOM_WIDTH = 50;
 
 module.exports.loop = function () {
+  console.log("I am alive!");
     // These are "Run Once" conditions:
     for(var name in Game.rooms) { // GOOD
       for(var spawn_name in Game.spawns) {
