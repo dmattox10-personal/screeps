4b910779272292d5f0cfe17b7b1d5175c3754735
Daniel Mattox <dmattox10@gmail.com>
2018-06-21 16:27:56 -0400

    minor change

--- dist/harvesterV3.js
+++ dist/harvesterV3.js
@@ -2,7 +2,7 @@ var harvesters = {
   spawn: (spawn_name) => {
     var name = 'harvester' + Game.time // TODO switch to UUID-Lib
     var components = [WORK,CARRY,MOVE,MOVE]
-    Game.spawns[spawn_name].createCreep(components, name, { memory: { role: 'harvester' }})
+    Game.spawns[spawn_name].spawnCreep(components, name, memory: { role: 'harvester' })
   },
   run: (creep) => {
       if(creep.carry.energy < creep.carryCapacity) {
