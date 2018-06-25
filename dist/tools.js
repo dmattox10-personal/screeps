859d78c7b846426c8f0fc71902347ec511327286
Daniel Mattox <dmattox10@gmail.com>
2018-06-25 15:52:11 -0400

    Minor changes

--- dist/tools.js
+++ dist/tools.js
@@ -5,7 +5,7 @@ var tools = {
       for (var spawn_name in Game.spawns) {
         let current_colony = Game.rooms[colony_name] // DNM
         let current_colony_name = current_colony.name // DNM
-        let sources = Game.rooms[current_colony].find(FIND_SOURCES);
+        let sources = Game.rooms[current_colony_name].find(FIND_SOURCES);
         console.log(current_colony_name)
       } // Game.spawns
     } // Game.rooms
