35f1d6bb8bb952279ef9c6d7305aea88ac032827
Daniel Mattox <dmattox10@gmail.com>
2018-06-25 14:41:42 -0400

    Minor changes

--- dist/tools.js
+++ dist/tools.js
@@ -4,8 +4,8 @@ var tools = {
     for (var room_name in Game.rooms) {
       let obj_name = room_name
       for (var spawn_name in Game.spawns) {
-        if (!Memory.rooms[obj_name]) {
-          Memory.rooms[obj_name] = room_name
+        if (!Memory.rooms[room_name]) {
+          //Memory.rooms[obj_name] = room_name
           Memory.rooms[room_name].sources = Game.rooms[room_name].find(FIND_SOURCES)
           // Memory.rooms[Memory.rooms.length] = room_name
           // Get all the info we need to save, don't map in here!
