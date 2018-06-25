da86d45b588adddf574c8ff1180a2569c690a18e
Daniel Mattox <dmattox10@gmail.com>
2018-06-25 16:20:25 -0400

    Minor changes

--- dist/tools.js
+++ dist/tools.js
@@ -5,16 +5,15 @@ var tools = {
       for (var spawn_name in Game.spawns) {
         let current_colony = Game.rooms[colony_name] // DNM
         let current_colony_name = current_colony.name // DNM
-        let sources = Game.rooms[current_colony_name].find(FIND_SOURCES); // DNM
         if (!Memory.colonies) {
-          Memory.colonies = {}
-          Memory.colonies.current_colony_name = {}
-          Memory.colonies.current_colony_name.id = 0
+          Memory.colonies = []
           console.log("colonies inserted into memory")
 
         }
-        if (!Memory.colonies.colony.current_colony_name) {
-          Memory.colonies.colony[Memory.colonies.length] = current_colony_name
+        if (!Memory.colonies[current_colony_name]) {
+          let sources = Game.rooms[current_colony_name].find(FIND_SOURCES); // DNM
+          current_colony_name.id = 0
+          colonies.push(current_colony_name)
           console.log("added colony " + current_colony_name + " to colonies object")
         }
       } // Game.spawns
