724f1ca9266620df5fe9da5f873c6db06280a8e0
Daniel Mattox <dmattox10@gmail.com>
2018-06-25 15:19:44 -0400

    Minor changes

--- dist/tools.js
+++ dist/tools.js
@@ -5,8 +5,9 @@ var tools = {
       for (var spawn_name in Game.spawns) {
         let current_room = Game.rooms[room_name]
         let current_room_name = current_room.name
-        console.log(current_room_name)
-        console.log(Game.rooms.current_room_name)
+        //console.log(current_room_name)
+        var sources = Game.rooms.current_room_name.find(FIND_SOURCES);
+        let Memory.rooms.current_room_name.sources = sources
       } // Game.spawns
     } // Game.rooms
   } // Setup
