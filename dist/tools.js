4880e9ec5d549a87077e1217b792e8dfe0b9e6cb
Daniel Mattox <dmattox10@gmail.com>
2018-06-25 14:06:16 -0400

    Adding room object to main memory

--- dist/tools.js
+++ dist/tools.js
@@ -1,19 +1,17 @@
 var tools = {
   setup: () => {
+
     for (var room_name in Game.rooms) {
       for (var spawn_name in Game.spawns) {
-        if (!Memory.rooms) {
-
-
-        }
-        if (!Memory.rooms[room_name]) {
-          Memory.rooms = {}
+        if (!Memory.rooms.room_name) {
+          let obj_name = room_name
+          var sources = Game.rooms[room_name].find(FIND_SOURCES)
+          console.log(sources)
+          // Memory.rooms.obj_name.sources = sources
           // Memory.rooms[Memory.rooms.length] = room_name
           // Get all the info we need to save, don't map in here!
-          var sources = Game.rooms[room_name].find(FIND_SOURCES)
-          //console.log(sources)
-          Memory.rooms.room_name.sources = sources
-        }
+          // console.log(sources)
+          // Memory.rooms.room_name.sources = sources
       } // Game.spawns
     } // Game.rooms
   }
