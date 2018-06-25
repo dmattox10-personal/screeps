3f723033de2b19c2ca747436cd3996fd9419fe1b
Daniel Mattox <dmattox10@gmail.com>
2018-06-25 14:54:58 -0400

    Minor changes

--- dist/tools.js
+++ dist/tools.js
@@ -2,11 +2,12 @@ var tools = {
   setup: () => {
 
     for (var room_name in Game.rooms) {
-      var local_name = room_name
+      console.log(room_name)
       for (var spawn_name in Game.spawns) {
-        if (!Memory.myRooms.local_name) {
-        Memory.myRooms.local_name = {}
-        Memory.myRooms.local_name.sources = Game.rooms.local_name.find(FIND_SOURCES)
+        if (!Memory.myRooms[room_name]) {
+          console.log(room_name)
+        // Memory.myRooms.local_name = {}
+      // Memory.myRooms.local_name.sources = Game.rooms.local_name.find(FIND_SOURCES)
           // Memory.rooms[Memory.rooms.length] = room_name
           // Get all the info we need to save, don't map in here!
           // console.log(sources)
