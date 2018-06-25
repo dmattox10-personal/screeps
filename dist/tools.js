6c62c9e42ec83624b42e3c07151c47ee9abd89fc
Daniel Mattox <dmattox10@gmail.com>
2018-06-25 11:39:39 -0400

    Moving FIND SOURCES out of the creep loop

--- dist/tools.js
+++ dist/tools.js
@@ -2,7 +2,7 @@ var tools = {
   setup: () => {
     for (var room_name in Game.rooms) {
       for (var spawn_name in Game.spawns) {
-        if (!Memory.rooms[room_name]) {
+        if (!Memory.rooms.room_name) {
           // Save the room name LAST after populating it's object with properties here!!!
           console.log(room_name.find(FIND_SOURCES))
         }
