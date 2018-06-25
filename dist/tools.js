f8c1e7bf39e898129f2598531a555303a286fa54
Daniel Mattox <dmattox10@gmail.com>
2018-06-25 16:28:30 -0400

    Minor changes

--- dist/tools.js
+++ dist/tools.js
@@ -12,8 +12,11 @@ var tools = {
         }
         if (!Memory.colonies[current_colony_name]) {
           let sources = Game.rooms[current_colony_name].find(FIND_SOURCES); // DNM
-          current_colony_name.id = 0
-          Memory.colonies.current_colony_name = current_colony_name
+          let colonyEntry = {}
+          colonyEntry.name = current_colony_name
+          colonyEntry.id = Memory.colonies.length
+          colonyEntry.sources = sources
+          Memory.colonies.push = colonyEntry
           console.log("added colony " + current_colony_name + " to colonies object")
         }
       } // Game.spawns
